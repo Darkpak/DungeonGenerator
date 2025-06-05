28473fac09338c0bdb381e71465970b1fb5b8db2
Darkpack <jaki5279@gmail.com>
2025-06-06 00:30:42 +0200

    Implemented room splitting method
    List for rooms and logic for splitting rooms horizontally

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,19 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    public bool splitHorizontally;
-    RectInt room = new RectInt(0, 0, 100, 50);
+    [Header("Dungeon Size")]
+    [SerializeField] private RectInt dungeonArea;
+    
+    [Header("Room Settings")]
+    [SerializeField] private List<RectInt> rooms = new List<RectInt>();
+    public int minRoomSize = 3;
+    
 
     void Start()
     { 
-        
+        //StartCoroutine(GenerateDungeon());
+        GenerateDungeon();
     }
 
-    void Update()
+   
+    [Button]
+    //public IEnumerator GenerateDungeon()
+    private void GenerateDungeon()
     {
-        AlgorithmsUtils.DebugRectInt(room, Color.red);
+        (RectInt roomA, RectInt roomB) = SplitHorizontally(dungeonArea);
+        rooms.Add(roomA);
+        rooms.Add(roomB);
+        DebugDrawingBatcher.BatchCall( () =>
+        {
+            AlgorithmsUtils.DebugRectInt(roomA, Color.red);
+            AlgorithmsUtils.DebugRectInt(roomB, Color.green);
+        });
+        //yield return new WaitForEndOfFrame();
+    }
+
+    private (RectInt, RectInt) SplitHorizontally(RectInt splitRoom)
+    {
+        RectInt roomA = splitRoom;
+        RectInt roomB = splitRoom;
+        roomA.height = (splitRoom.height / 2) + Random.Range(0, splitRoom.height);
+        roomB.height -= (roomA.height - 1);
+        roomB.y += roomA.height - 1;
+        return (roomA, roomB);
     }
 }
